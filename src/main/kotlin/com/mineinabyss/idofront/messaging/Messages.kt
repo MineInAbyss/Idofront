64db37e3cec9817e548fa77a8ed435163b1a36d0
0ffz <dan.voznyy@gmail.com>
2020-12-13 18:33:45 -0500

    Change messages to take Any? instead of String

--- src/main/kotlin/com/mineinabyss/idofront/messaging/Messages.kt
+++ src/main/kotlin/com/mineinabyss/idofront/messaging/Messages.kt
@@ -32,41 +32,41 @@ fun <T> T.logVal(message: String = ""): T = logInfo("$message${if (message == ""
 /**
  * Runs [Bukkit.broadcastMessage].
  */
-fun broadcast(message: String) = Bukkit.broadcastMessage(message)
+fun broadcast(message: Any?) = Bukkit.broadcastMessage(message.toString())
 
 
-inline fun logTo(message: String, colorChar: Char? = null, printTo: (String) -> Unit) =
-        printTo(message.color(colorChar))
+inline fun logTo(message: Any?, colorChar: Char? = null, printTo: (String) -> Unit) =
+        printTo(message.toString().color(colorChar))
 
-fun logInfo(message: String, colorChar: Char? = null) =
+fun logInfo(message: Any?, colorChar: Char? = null) =
         logTo(message, colorChar, Bukkit.getLogger()::info)
 
-fun logError(message: String, colorChar: Char? = null) =
+fun logError(message: Any?, colorChar: Char? = null) =
         logTo(message, colorChar, Bukkit.getLogger()::severe)
 
-fun logSuccess(message: String, colorChar: Char? = null) =
+fun logSuccess(message: Any?, colorChar: Char? = null) =
         logTo("${ChatColor.GREEN}$message", colorChar, Bukkit.getLogger()::info)
 
-fun logWarn(message: String, colorChar: Char? = null) =
+fun logWarn(message: Any?, colorChar: Char? = null) =
         logTo(message, colorChar, Bukkit.getLogger()::warning)
 
 private val ERROR_PREFIX = "&4&l\u274C&c".color()
 private val SUCCESS_PREFIX = "&a&l\u2714&a".color()
 private val WARN_PREFIX = "&e\u26A0&7".color()
 
-fun CommandSender.info(message: String, colorChar: Char? = null) = logTo(message, colorChar, ::sendMessage)
+fun CommandSender.info(message: Any?, colorChar: Char? = null) = logTo(message, colorChar, ::sendMessage)
 
-fun CommandSender.error(message: String, colorChar: Char? = null) {
+fun CommandSender.error(message: Any?, colorChar: Char? = null) {
     if (this is ConsoleCommandSender) logError(message, colorChar)
     else info("$ERROR_PREFIX $message", colorChar)
 }
 
-fun CommandSender.success(message: String, colorChar: Char? = null) {
+fun CommandSender.success(message: Any?, colorChar: Char? = null) {
     if (this is ConsoleCommandSender) logSuccess(message, colorChar)
     else info("$SUCCESS_PREFIX $message", colorChar)
 }
 
-fun CommandSender.warn(message: String, colorChar: Char? = null) {
+fun CommandSender.warn(message: Any?, colorChar: Char? = null) {
     if (this is ConsoleCommandSender) logWarn(message, colorChar)
     else info("$WARN_PREFIX $message", colorChar)
 }
