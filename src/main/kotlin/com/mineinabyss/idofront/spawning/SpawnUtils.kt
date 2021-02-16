6d2df464aa2008ef58576ffcbcf0761b1978e823
0ffz <dan.voznyy@gmail.com>
2021-02-16 16:25:29 -0500

    Spawning via EntityType

--- src/main/kotlin/com/mineinabyss/idofront/spawning/SpawnUtils.kt
+++ src/main/kotlin/com/mineinabyss/idofront/spawning/SpawnUtils.kt
@@ -2,7 +2,13 @@ package com.mineinabyss.idofront.spawning
 
 import org.bukkit.Location
 import org.bukkit.entity.Entity
+import org.bukkit.entity.EntityType
 
 inline fun <reified T : Entity> Location.spawn(): T? = world?.spawn(this, T::class.java)
 
 inline fun <reified T : Entity> Location.spawn(init: T.() -> Unit): T? = spawn<T>()?.apply { init() }
+
+fun Location.spawn(type: EntityType): Entity? = world?.spawnEntity(this, type)
+
+fun Location.spawn(type: EntityType, init: Entity.() -> Unit): Entity? = spawn(type)?.apply { init() }
+
