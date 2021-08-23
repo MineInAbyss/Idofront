70d8239669e7381cd1037735b8ae9b9adbe3d7d2
Scyu_ <scyu@scyu.dev>
2021-08-24 00:47:12 +0100

    add group to recipes

--- src/main/kotlin/com/mineinabyss/idofront/serialization/SerializableRecipe.kt
+++ src/main/kotlin/com/mineinabyss/idofront/serialization/SerializableRecipe.kt
@@ -2,7 +2,6 @@ package com.mineinabyss.idofront.serialization
 
 import com.mineinabyss.idofront.util.toMCKey
 import kotlinx.serialization.Serializable
-import org.bukkit.Bukkit
 import org.bukkit.NamespacedKey
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.Recipe
@@ -14,10 +13,12 @@ class SerializableRecipeIngredients(
     val items: Map<String, SerializableItemStack>,
     val configuration: String,
 ) {
-    fun toCraftingRecipe(key: NamespacedKey, result: ItemStack): Recipe {
+    fun toCraftingRecipe(key: NamespacedKey, result: ItemStack, group: String): Recipe {
         val recipe = ShapedRecipe(key, result)
 
         recipe.shape(*configuration.replace("|", "").split("\n").toTypedArray())
+
+        recipe.group = group
 
         items.forEach { (key, ingredient) ->
             recipe.setIngredient(key[0], RecipeChoice.ExactChoice(ingredient.toItemStack()))
@@ -32,7 +33,8 @@ class SerializableRecipe(
     val key: String,
     val ingredients: SerializableRecipeIngredients,
     val result: SerializableItemStack,
+    val group: String = "",
 ) {
     fun toCraftingRecipe() =
-        ingredients.toCraftingRecipe(key.toMCKey(), result.toItemStack())
+        ingredients.toCraftingRecipe(key.toMCKey(), result.toItemStack(), group)
 }
