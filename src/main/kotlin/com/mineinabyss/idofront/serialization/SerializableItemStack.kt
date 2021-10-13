5d2852da3a5bf94e88ff38eca9ecc3a2c0e2f8ae
Scyu_ <scyu@scyu.dev>
2021-10-13 01:20:48 +0100

    Update recipes (#22)

--- src/main/kotlin/com/mineinabyss/idofront/serialization/SerializableItemStack.kt
+++ src/main/kotlin/com/mineinabyss/idofront/serialization/SerializableItemStack.kt
@@ -4,6 +4,7 @@ import com.mineinabyss.idofront.plugin.getService
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import org.bukkit.Material
+import org.bukkit.inventory.ItemFlag
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.Damageable
 
@@ -24,7 +25,7 @@ open class SerializableItemStack(
     var lore: String? = null,
     var damage: Int? = null,
     var prefab: String? = null,
-    var hideItemFlags: Boolean = false,
+    var hideItemFlags: List<ItemFlag> = listOf(),
     var tag: String = ""
 ) {
     open fun toItemStack(applyTo: ItemStack = ItemStack(type ?: Material.AIR)) = applyTo.apply {
@@ -35,6 +36,8 @@ open class SerializableItemStack(
         }
         val meta = itemMeta ?: return@apply
 
+        amount = this@SerializableItemStack.amount
+
         this@SerializableItemStack.type?.let { type = it }
         customModelData?.let { meta.setCustomModelData(it) }
         displayName?.let { meta.setDisplayName(it) }
@@ -42,7 +45,7 @@ open class SerializableItemStack(
         unbreakable?.let { meta.isUnbreakable = it }
         this@SerializableItemStack.lore?.let { meta.lore = it.split("\n") }
         if (this is Damageable) this@SerializableItemStack.damage?.let { damage = it }
-        if (hideItemFlags) meta.removeItemFlags()
+        if (hideItemFlags.isNotEmpty()) meta.addItemFlags(*hideItemFlags.toTypedArray())
 
         itemMeta = meta
     }
