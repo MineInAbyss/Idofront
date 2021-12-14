a7730a4dc7d3b6da3bf2baf3b2ff858bfb5acf74
0ffz <dan.voznyy@gmail.com>
2021-12-14 16:46:57 -0500

    Support positive spaces

--- src/main/kotlin/com/mineinabyss/idofront/font/Space.kt
+++ src/main/kotlin/com/mineinabyss/idofront/font/Space.kt
@@ -1,6 +1,8 @@
 package com.mineinabyss.idofront.font
 
-object NegativeSpace {
+import kotlin.math.abs
+
+object Space {
     const val MINUS_1 = "\uF801"
     const val MINUS_2 = "\uF802"
     const val MINUS_4 = "\uF804"
@@ -13,20 +15,37 @@ object NegativeSpace {
     const val MINUS_512 = "\uF80E"
     const val MINUS_1024 = "\uF80F"
 
-    val powers = listOf(
+    const val PLUS_1 = "\uF821"
+    const val PLUS_2 = "\uF822"
+    const val PLUS_4 = "\uF824"
+    const val PLUS_8 = "\uF828"
+    const val PLUS_16 = "\uF829"
+    const val PLUS_32 = "\uF82A"
+    const val PLUS_64 = "\uF82B"
+    const val PLUS_128 = "\uF82C"
+    const val PLUS_256 = "\uF82D"
+    const val PLUS_512 = "\uF82E"
+    const val PLUS_1024 = "\uF82F"
+
+    private val powers_minus = listOf(
         // Start with power of 0
         "", MINUS_1, MINUS_2, MINUS_4,
         MINUS_8, MINUS_16, MINUS_32, MINUS_64,
         MINUS_128, MINUS_256, MINUS_512, MINUS_1024
     )
 
+    private val powers_plus = listOf(
+        "", PLUS_1, PLUS_2, PLUS_4,
+        PLUS_8, PLUS_16, PLUS_32, PLUS_64,
+        PLUS_128, PLUS_256, PLUS_512, PLUS_1024
+    )
+
     fun of(shift: Int) = buildString {
+        val powers = if (shift > 0) powers_plus else powers_minus
         repeat(powers.size) { i ->
             val pow = i + 1
             val bit = 1 shl (i)
-            println(pow)
-            println(bit)
-            if (shift and bit != 0)
+            if (abs(shift) and bit != 0)
                 append(powers[pow])
         }
     }
---
+++ src/test/kotlin/com/mineinabyss/idofront/font/SpaceTest.kt
@@ -0,0 +1,15 @@
+package com.mineinabyss.idofront.font
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+internal class SpaceTest {
+    @Test
+    fun of() {
+        Space.of(-1) shouldBe Space.MINUS_1
+        Space.of(-1024) shouldBe Space.MINUS_1024
+        Space.of(-11) shouldBe "${Space.MINUS_1}${Space.MINUS_2}${Space.MINUS_8}"
+        Space.of(11) shouldBe "${Space.PLUS_1}${Space.PLUS_2}${Space.PLUS_8}"
+        Space.of(0) shouldBe ""
+    }
+}
