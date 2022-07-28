ffbf82049b9d258d73afbf374c1eede51217f1a9
Boy <sivertpaulsen2@gmail.com>
2022-07-28 21:26:20 +0200

    changes

--- src/main/kotlin/com/mineinabyss/idofront/serialization/PotionEffectSerializer.kt
+++ src/main/kotlin/com/mineinabyss/idofront/serialization/PotionEffectSerializer.kt
@@ -1,62 +1,52 @@
 package com.mineinabyss.idofront.serialization
 
-import com.mineinabyss.idofront.time.inWholeTicks
-import com.mineinabyss.idofront.time.ticks
 import kotlinx.serialization.KSerializer
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.descriptors.buildClassSerialDescriptor
-import kotlinx.serialization.descriptors.element
-import kotlinx.serialization.encoding.*
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
 
+@Serializable
+@SerialName("PotionEffect")
+private class PotionEffectSurrogate(
+    val type: String,
+    val duration: Long,
+    val amplifier: Int = 0,
+    val isAmbient: Boolean = true,
+    val hasParticles: Boolean = true,
+    val hasIcon: Boolean = true
+) {
+    init {
+        require(type.isNotEmpty() && duration > 0L)
+    }
+}
+
 object PotionEffectSerializer : KSerializer<PotionEffect> {
-    override val descriptor: SerialDescriptor = buildClassSerialDescriptor("Location") {
-        element<String>("type")
-        element("duration", DurationSerializer.descriptor)
-        element<Int>("amplifier")
-        element<Boolean>("ambient")
-        element<Boolean>("particles")
-        element<Boolean>("icon")
+    override val descriptor: SerialDescriptor = PotionEffectSurrogate.serializer().descriptor
+    override fun serialize(encoder: Encoder, value: PotionEffect) {
+        val surrogate = PotionEffectSurrogate(
+            value.type.name,
+            value.duration.toLong(),
+            value.amplifier,
+            value.isAmbient,
+            value.hasParticles(),
+            value.hasIcon()
+        )
+        encoder.encodeSerializableValue(PotionEffectSurrogate.serializer(), surrogate)
     }
 
-    override fun serialize(encoder: Encoder, value: PotionEffect) =
-        encoder.encodeStructure(descriptor) {
-            encodeStringElement(descriptor, 0, value.type.toString())
-            encodeSerializableElement(descriptor, 1, DurationSerializer, value.duration.ticks)
-            encodeIntElement(descriptor, 2, value.amplifier)
-            encodeBooleanElement(descriptor, 3, value.isAmbient)
-            encodeBooleanElement(descriptor, 4, value.hasParticles())
-            encodeBooleanElement(descriptor, 5, value.hasIcon())
-        }
-
     override fun deserialize(decoder: Decoder): PotionEffect {
-        var type = ""
-        var duration = 1.ticks
-        var amplifier = 1
-        var isAmbient = true
-        var hasParticles = true
-        var hasIcon = true
-        decoder.decodeStructure(descriptor) {
-            loop@ while (true) {
-                when (val i = decodeElementIndex(descriptor)) {
-                    0 -> type = decodeStringElement(descriptor, i)
-                    1 -> duration = decodeSerializableElement(descriptor, i, DurationSerializer)
-                    2 -> amplifier = decodeIntElement(descriptor, i)
-                    3 -> isAmbient = decodeBooleanElement(descriptor, i)
-                    4 -> hasParticles = decodeBooleanElement(descriptor, i)
-                    5 -> hasIcon = decodeBooleanElement(descriptor, i)
-                    CompositeDecoder.DECODE_DONE -> break
-                }
-            }
-        }
+        val surrogate = decoder.decodeSerializableValue(PotionEffectSurrogate.serializer())
         return PotionEffect(
-            PotionEffectType.getByName(type) ?: error("$type is not a valid potion effect type"),
-            duration.inWholeTicks.toInt(),
-            amplifier,
-            isAmbient,
-            hasParticles,
-            hasIcon
+            PotionEffectType.getByName(surrogate.type)!!,
+            surrogate.duration.toInt(),
+            surrogate.amplifier,
+            surrogate.isAmbient,
+            surrogate.hasParticles,
+            surrogate.hasIcon
         )
     }
 }
