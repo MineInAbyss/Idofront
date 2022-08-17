3e92ab6e91540182a0551d4367009c5b4653a8b1
Boy <sivertpaulsen2@gmail.com>
2022-08-18 01:33:07 +0200

    itemstackserializer

--- src/main/kotlin/com/mineinabyss/idofront/serialization/ItemStackSerializer.kt
+++ src/main/kotlin/com/mineinabyss/idofront/serialization/ItemStackSerializer.kt
@@ -1,28 +1,33 @@
 package com.mineinabyss.idofront.serialization
 
+import com.mineinabyss.idofront.items.editItemMeta
 import kotlinx.serialization.KSerializer
-import kotlinx.serialization.descriptors.PrimitiveKind
-import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
-import org.bukkit.configuration.MemorySection
-import org.bukkit.configuration.file.YamlConfiguration
+import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.ItemMeta
 
-class ItemStackSerializer : KSerializer<ItemStack> {
-    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("itemstack", PrimitiveKind.STRING)
+@Serializable
+@SerialName("ItemStack")
+private class ItemStackSurrogate(
+    val type: Material,
+    val amount: Int,
+    val meta: ItemMeta,
+)
 
-    override fun deserialize(decoder: Decoder): ItemStack {
-
-        return ItemStack.deserialize((YamlConfiguration().apply {
-            loadFromString(decoder.decodeString())
-        }.get("i") as MemorySection).getValues(true)) //TODO try to encode this at the root level
-    }
+object ItemStackSerializer : KSerializer<ItemStack> {
+    override val descriptor: SerialDescriptor = ItemStackSurrogate.serializer().descriptor
 
     override fun serialize(encoder: Encoder, value: ItemStack) {
-        encoder.encodeString(YamlConfiguration().apply {
-            set("i", value.serialize())
-        }.saveToString())
+        encoder.encodeSerializableValue(ItemStackSurrogate.serializer(), ItemStackSurrogate(value.type, value.amount, value.itemMeta))
+    }
+
+    override fun deserialize(decoder: Decoder): ItemStack {
+        val surrogate = decoder.decodeSerializableValue(ItemStackSurrogate.serializer())
+        return ItemStack(surrogate.type, surrogate.amount).editItemMeta { surrogate.meta }
     }
 }
