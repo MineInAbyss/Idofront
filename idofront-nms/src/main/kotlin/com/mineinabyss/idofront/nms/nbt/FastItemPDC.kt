32385ab0ed40b8829846e9c93ff931a8bf9c41ef
0ffz <dan.voznyy@gmail.com>
2022-08-20 21:39:26 -0400

    Memoize toMCKey to avoid going through regex, remove useless has check for fast PDC

--- idofront-nms/src/main/kotlin/com/mineinabyss/idofront/nms/nbt/FastItemPDC.kt
+++ idofront-nms/src/main/kotlin/com/mineinabyss/idofront/nms/nbt/FastItemPDC.kt
@@ -5,5 +5,9 @@ import net.minecraft.nbt.CompoundTag
 import org.bukkit.inventory.ItemStack
 
 val net.minecraft.world.item.ItemStack.hasPDC: Boolean get() = tag?.contains("PublicBukkitValues") == true
-val net.minecraft.world.item.ItemStack.fastPDC get() = if (hasPDC) WrappedPDC(tag?.get("PublicBukkitValues") as CompoundTag) else null
+val net.minecraft.world.item.ItemStack.fastPDC: WrappedPDC?
+    get() {
+        return WrappedPDC((tag?.get("PublicBukkitValues") ?: return null) as CompoundTag)
+    }
+
 val ItemStack.fastPDC get() = toNMS()?.fastPDC
--- src/main/kotlin/com/mineinabyss/idofront/util/NameSpacedKeyHelpers.kt
+++ src/main/kotlin/com/mineinabyss/idofront/util/NameSpacedKeyHelpers.kt
@@ -1,15 +1,19 @@
 package com.mineinabyss.idofront.util
 
+import com.google.common.cache.CacheBuilder
 import org.bukkit.NamespacedKey
+
+// Bukkit uses a regex in the constructor to validate keys 💀
+private val memoizedKeys = CacheBuilder.newBuilder().maximumSize(100).build<String, NamespacedKey>()
 
 /** Converts this string to a [NamespacedKey] where the string must be formatted as `namespace:key` */
 fun String.toMCKey(): NamespacedKey {
+    memoizedKeys.getIfPresent(this)?.let { return it }
     val split = split(':')
     if (split.size != 2)
         error("Malformed key: $this, must only contain one : that splits namespace and key.")
 
     val (namespace, key) = split
 
-    @Suppress("DEPRECATION") // deprecated just to discourage using instantiating without plugin reference
-    return NamespacedKey(namespace, key)
+    return NamespacedKey(namespace, key).also { memoizedKeys.put(this, it) }
 }
