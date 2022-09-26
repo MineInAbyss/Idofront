146d399718c14a9cc7867804cae69d9ed28f361f
Boy0000 <sivertpaulsen2@gmail.com>
2022-09-26 16:23:17 +0200

    add more adventure stuff (#44)
    * add more adventure stuff
    
    * merge .miniMsg methods

--- src/main/kotlin/com/mineinabyss/idofront/messaging/Messages.kt
+++ src/main/kotlin/com/mineinabyss/idofront/messaging/Messages.kt
@@ -4,12 +4,15 @@ package com.mineinabyss.idofront.messaging
 
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.minimessage.MiniMessage
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
 import org.bukkit.Bukkit
 import org.bukkit.command.CommandSender
 import org.bukkit.command.ConsoleCommandSender
 
 @PublishedApi
 internal val mm = MiniMessage.miniMessage()
+internal val plain = PlainTextComponentSerializer.plainText()
 private const val ERROR_PREFIX = "<dark_red><b>\u274C</b><red>"
 private const val SUCCESS_PREFIX = "<green><b>\u2714</b>"
 private const val WARN_PREFIX = "<yellow>\u26A0<gray>"
@@ -51,11 +54,17 @@ fun CommandSender.warn(message: Any?) {
     else info("$WARN_PREFIX $message")
 }
 
-/** Parses this String to a [Component] with MiniMessage */
-fun String.miniMsg(): Component = mm.deserialize(this)
+/** Parses this String to a [Component] with MiniMessage and an optional TagResolver */
+fun String.miniMsg(tagResolver: TagResolver = TagResolver.standard()): Component = mm.deserialize(this, tagResolver)
 
 /** Serializes this [Component] to a String with MiniMessage */
 fun Component.serialize(): String = mm.serialize(this)
+
+/** Serializes this [Component] to a plain text string */
+fun Component.toPlainText(): String = plain.serialize(this)
+
+/** Removes all supported tags from a string, with an optional TagResolver input */
+fun String.stripTags(tagResolver: TagResolver = TagResolver.standard()): String = mm.stripTags(this, tagResolver)
 
 /**
  * (Kotlin) Logs a value with an optional string in front of it e.x.
