2662e789c2f7f9df5e5ead44e46494927b9f370c
0ffz <dan.voznyy@gmail.com>
2022-09-26 21:24:11 -0400

    Handle edge cases when trying to find next version

--- idofront-gradle/src/main/kotlin/com.mineinabyss.conventions.autoversion.gradle.kts
+++ idofront-gradle/src/main/kotlin/com.mineinabyss.conventions.autoversion.gradle.kts
@@ -1,14 +1,21 @@
 val releaseVersion: String? = System.getenv("RELEASE_VERSION")
 
-version = (if (releaseVersion != null) {
-    val (major, minor, patch) = releaseVersion.split(".")
-    val (majorTarget, minorTarget) = version.toString().split(".")
-    if (majorTarget == major && minorTarget == minor) {
-        "$major.$minor.${patch.toInt() + 1}"
-    } else {
-        "$majorTarget.$minorTarget.0"
-    }
-} else "$version-dev")
+fun getNextVersion(): String {
+    if (releaseVersion != null) {
+        val (majorTarget, minorTarget) = version.toString().split(".")
+        try {
+            val (major, minor, patch) = releaseVersion.removePrefix("v").split(".")
+            if (majorTarget == major && minorTarget == minor) {
+                return "$major.$minor.${patch.toInt() + 1}"
+            }
+        } catch (_: Exception) {
+        }
+        return "$majorTarget.$minorTarget.0"
+    } else return "$version-dev"
+}
+
+version = getNextVersion()
+
 
 subprojects {
     version = rootProject.version
