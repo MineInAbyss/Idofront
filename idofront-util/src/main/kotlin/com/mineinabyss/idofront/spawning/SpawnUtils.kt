602fc9b0b0f284d64125e5872b6cc6667da6f9e5
Boy <sivertpaulsen2@gmail.com>
2022-11-20 15:37:04 +0100

    make spawn method call the init pre spawning instead of after

--- idofront-util/src/main/kotlin/com/mineinabyss/idofront/spawning/SpawnUtils.kt
+++ idofront-util/src/main/kotlin/com/mineinabyss/idofront/spawning/SpawnUtils.kt
@@ -6,7 +6,7 @@ import org.bukkit.entity.EntityType
 
 inline fun <reified T : Entity> Location.spawn(): T? = world?.spawn(this, T::class.java)
 
-inline fun <reified T : Entity> Location.spawn(init: T.() -> Unit): T? = spawn<T>()?.apply { init() }
+inline fun <reified T : Entity> Location.spawn(noinline init: T.() -> Unit): T? = world?.spawn(this, T::class.java, init)
 
 fun Location.spawn(type: EntityType): Entity? = world?.spawnEntity(this, type)
 
