2fbe835c54336eb98c796186ac1dd95076994f4e
0ffz <dan.voznyy@gmail.com>
2023-04-02 22:10:47 -0400

    Conditionally publish to snapshots repo if version ends with SNAPSHOT

--- idofront-gradle/src/main/kotlin/Publishing.kt
+++ idofront-gradle/src/main/kotlin/Publishing.kt
@@ -1,12 +1,13 @@
 import org.gradle.api.Project
 import org.gradle.api.publish.PublishingExtension
-import org.gradle.kotlin.dsl.maven
 
 fun PublishingExtension.addMineInAbyssRepo(
     project: Project,
 ) {
     repositories {
-        maven("https://repo.mineinabyss.com/releases") {
+        maven {
+            val repo = "https://repo.mineinabyss.com/"
+            setUrl(if (project.version.toString().endsWith("SNAPSHOT")) repo + "snapshots" else repo + "releases")
             credentials {
                 username = project.findProperty("mineinabyssMavenUsername") as String?
                 password = project.findProperty("mineinabyssMavenPassword") as String?
