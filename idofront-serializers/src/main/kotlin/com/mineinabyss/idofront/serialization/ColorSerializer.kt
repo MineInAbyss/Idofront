568060e8a86668b0f355f5876c6c5a387d1d0127
Boy <sivertpaulsen2@gmail.com>
2023-06-13 12:06:00 +0200

    make ColorSerializer support hex

--- idofront-serializers/src/main/kotlin/com/mineinabyss/idofront/serialization/ColorSerializer.kt
+++ idofront-serializers/src/main/kotlin/com/mineinabyss/idofront/serialization/ColorSerializer.kt
@@ -1,5 +1,6 @@
 package com.mineinabyss.idofront.serialization
 
+import com.mineinabyss.idofront.messaging.logWarn
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -14,8 +15,14 @@ import org.bukkit.Color
 private value class ColorSurrogate(val color: String) {
     init {
         val split = color.splitColor()
-        require(split.size == 3) { "Color must be in the form of 'r, g, b'" }
-        require(split.all { it in 0..255 }) { "Color must be in the range of 0-255" }
+        if (color.startsWith("#")) {
+            logWarn("Color is in the form of '#rrggbb'")
+            require(color.length == 7) { "Color must be in the form of '#rrggbb'" }
+        } else if (',' in color) {
+            logWarn("Color is in the form of 'r, g, b'")
+            require(split.size == 3) { "Color must be in the form of 'r, g, b'" }
+            require(split.all { it in 0..255 }) { "Color must be in the range of 0-255" }
+        }
     }
 }
 
@@ -27,8 +34,10 @@ object ColorSerializer : KSerializer<Color> {
     }
 
     override fun deserialize(decoder: Decoder): Color {
-        val surrogate = decoder.decodeSerializableValue(ColorSurrogate.serializer()).color.splitColor()
-        return Color.fromRGB(surrogate[0], surrogate[1], surrogate[2])
+        val surrogate = decoder.decodeSerializableValue(ColorSurrogate.serializer()).color
+        return if (surrogate.startsWith("#")) Color.fromRGB(surrogate.substring(1).toInt(16))
+        else surrogate.splitColor().toIntArray().let { Color.fromRGB(it[0], it[1], it[2]) }
+
     }
 }
 
