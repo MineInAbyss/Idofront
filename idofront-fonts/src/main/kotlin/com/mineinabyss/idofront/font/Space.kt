cd1d9391b8e43679c3f60ec17241866d746ea5e9
Boy <sivertpaulsen2@gmail.com>
2023-10-29 21:41:01 +0100

    rework Space class

--- idofront-fonts/src/main/kotlin/com/mineinabyss/idofront/font/Space.kt
+++ idofront-fonts/src/main/kotlin/com/mineinabyss/idofront/font/Space.kt
@@ -2,61 +2,68 @@ package com.mineinabyss.idofront.font
 
 import kotlin.math.abs
 
-object Space {
-    const val MINUS_32768 = "\uE100"
-    const val MINUS_1 = "\uE101"
-    const val MINUS_2 = "\uE102"
-    const val MINUS_3 = "\uE103"
-    const val MINUS_4 = "\uE104"
-    const val MINUS_5 = "\uE105"
-    const val MINUS_6 = "\uE106"
-    const val MINUS_7 = "\uE107"
-    const val MINUS_8 = "\uE108"
-    const val MINUS_16 = "\uE109"
-    const val MINUS_32 = "\uE110"
-    const val MINUS_64 = "\uE111"
-    const val MINUS_128 = "\uE112"
-    const val MINUS_256 = "\uE113"
-    const val MINUS_512 = "\uE114"
-    const val MINUS_1024 = "\uE115"
+enum class Space(val unicode: String) {
+    MINUS_0(""),
+    MINUS_1("\uE101"),
+    MINUS_2("\uE102"),
+    MINUS_4("\uE103"),
+    MINUS_8("\uE104"),
+    MINUS_16("\uE105"),
+    MINUS_32("\uE106"),
+    MINUS_64("\uE107"),
+    MINUS_128("\uE108"),
+    MINUS_256("\uE109"),
+    MINUS_512("\uE110"),
+    MINUS_1024("\uE111"),
 
-    const val PLUS_32768 = "\uE116"
-    const val PLUS_1 = "\uE117"
-    const val PLUS_2 = "\uE118"
-    const val PLUS_3 = "\uE119"
-    const val PLUS_4 = "\uE120"
-    const val PLUS_5 = "\uE121"
-    const val PLUS_6 = "\uE122"
-    const val PLUS_7 = "\uE123"
-    const val PLUS_8 = "\uE124"
-    const val PLUS_16 = "\uE125"
-    const val PLUS_32 = "\uE126"
-    const val PLUS_64 = "\uE127"
-    const val PLUS_128 = "\uE128"
-    const val PLUS_256 = "\uE129"
-    const val PLUS_512 = "\uE130"
-    const val PLUS_1024 = "\uE131"
+    PLUS_0(""),
+    PLUS_1("\uE112"),
+    PLUS_2("\uE113"),
+    PLUS_4("\uE114"),
+    PLUS_8("\uE115"),
+    PLUS_16("\uE116"),
+    PLUS_32("\uE117"),
+    PLUS_64("\uE118"),
+    PLUS_128("\uE119"),
+    PLUS_256("\uE120"),
+    PLUS_512("\uE121"),
+    PLUS_1024("\uE122");
 
-    private val powers_minus = listOf(
-        // Start with power of 0
-        "", MINUS_1, MINUS_2, MINUS_4,
-        MINUS_8, MINUS_16, MINUS_32, MINUS_64,
-        MINUS_128, MINUS_256, MINUS_512, MINUS_1024
-    )
+    override fun toString() = unicode
 
-    private val powers_plus = listOf(
-        "", PLUS_1, PLUS_2, PLUS_4,
-        PLUS_8, PLUS_16, PLUS_32, PLUS_64,
-        PLUS_128, PLUS_256, PLUS_512, PLUS_1024
-    )
+    companion object {
+        private val powers_minus = listOf(
+            // Start with power of 0
+            MINUS_0, MINUS_1, MINUS_2, MINUS_4,
+            MINUS_8, MINUS_16, MINUS_32, MINUS_64,
+            MINUS_128, MINUS_256, MINUS_512, MINUS_1024
+        )
 
-    fun of(shift: Int) = buildString {
-        val powers = if (shift > 0) powers_plus else powers_minus
-        repeat(powers.size) { i ->
-            val pow = i + 1
-            val bit = 1 shl (i)
-            if (abs(shift) and bit != 0)
-                append(powers[pow])
+        private val powers_plus = listOf(
+            PLUS_0, PLUS_1, PLUS_2, PLUS_4,
+            PLUS_8, PLUS_16, PLUS_32, PLUS_64,
+            PLUS_128, PLUS_256, PLUS_512, PLUS_1024
+        )
+
+        fun Space.toNumber(): Int {
+            return when (name.substringBefore("_")) {
+                "PLUS" -> name.substringAfter("_").toIntOrNull() ?: 0
+                "MINUS" -> -(name.substringAfter("_").toIntOrNull() ?: 0)
+                else -> 0
+            }
+        }
+
+        fun of(space: Int) = buildString {
+            var remainingShift = space
+            val sortedSpaces = (if (space > 0) powers_plus else powers_minus).sortedByDescending { abs(it.toNumber()) }
+
+            sortedSpaces.forEach {
+                while (remainingShift * it.toNumber() > 0 && abs(it.toNumber()) <= abs(remainingShift)) {
+                    append(it.unicode)
+                    remainingShift -= it.toNumber()
+                    if (remainingShift == 0) break
+                }
+            }
         }
     }
 }
--- idofront-fonts/src/test/kotlin/com/mineinabyss/idofront/font/SpaceTest.kt
+++ idofront-fonts/src/test/kotlin/com/mineinabyss/idofront/font/SpaceTest.kt
@@ -6,10 +6,11 @@ import org.junit.jupiter.api.Test
 internal class SpaceTest {
     @Test
     fun of() {
-        Space.of(-1) shouldBe Space.MINUS_1
-        Space.of(-1024) shouldBe Space.MINUS_1024
-        Space.of(-11) shouldBe "${Space.MINUS_1}${Space.MINUS_2}${Space.MINUS_8}"
-        Space.of(11) shouldBe "${Space.PLUS_1}${Space.PLUS_2}${Space.PLUS_8}"
+        Space.of(-1) shouldBe "${Space.MINUS_1}"
+        Space.of(-1024) shouldBe "${Space.MINUS_1024}"
+        Space.of(-11) shouldBe "${Space.MINUS_8}${Space.MINUS_2}${Space.MINUS_1}"
+        Space.of(11) shouldBe "${Space.PLUS_8}${Space.PLUS_2}${Space.PLUS_1}"
+        Space.of(2048) shouldBe "${Space.PLUS_1024}${Space.PLUS_1024}"
         Space.of(0) shouldBe ""
     }
 }
