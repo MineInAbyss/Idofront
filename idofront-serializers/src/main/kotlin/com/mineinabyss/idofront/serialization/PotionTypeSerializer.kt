6961fe3afb4f02f2ae8103771c24bd2746931d46
Danielle Voznyy <dan.voznyy@gmail.com>
2024-01-17 22:07:56 -0500

    Support - in range serializers, fix potion type serializer to take string instead of nested structure

--- idofront-serializers/src/main/kotlin/com/mineinabyss/idofront/serialization/PotionTypeSerializer.kt
+++ idofront-serializers/src/main/kotlin/com/mineinabyss/idofront/serialization/PotionTypeSerializer.kt
@@ -1,26 +1,25 @@
 package com.mineinabyss.idofront.serialization
 
 import kotlinx.serialization.KSerializer
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+import org.bukkit.NamespacedKey
+import org.bukkit.Registry
 import org.bukkit.potion.PotionType
 
-@Serializable
-@SerialName("PotionType")
-private class PotionTypeSurrogate(val type: PotionType)
-
 object PotionTypeSerializer : KSerializer<PotionType> {
-    override val descriptor: SerialDescriptor = PotionTypeSurrogate.serializer().descriptor
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("PotionType", PrimitiveKind.STRING)
 
     override fun serialize(encoder: Encoder, value: PotionType) {
-        encoder.encodeSerializableValue(PotionTypeSurrogate.serializer(), PotionTypeSurrogate(value))
+        encoder.encodeString(value.key.asString())
     }
 
     override fun deserialize(decoder: Decoder): PotionType {
-        val surrogate = decoder.decodeSerializableValue(PotionTypeSurrogate.serializer())
-        return surrogate.type
+        val name = decoder.decodeString()
+        val type = NamespacedKey.fromString(name) ?: throw IllegalArgumentException("Invalid potion type: $name")
+        return Registry.POTION.get(type) ?: throw IllegalArgumentException("Invalid potion type: $name")
     }
 }
--- idofront-serializers/src/main/kotlin/com/mineinabyss/idofront/serialization/RangeSerializers.kt
+++ idofront-serializers/src/main/kotlin/com/mineinabyss/idofront/serialization/RangeSerializers.kt
@@ -67,9 +67,12 @@ object FloatRangeSerializer : RangeSerializer<FloatRange>() {
 }
 
 private inline fun <T> valuesForRange(string: String, map: String.() -> T): Pair<T, T> {
-    val range = string.split("..", " to ").map(map)
+    val range =
+        (if (!string.startsWith("-")) string.split("..", " to ", "-")
+        else string.split("..", " to "))
+            .map(map)
 
-    if (range.size > 2) error("Malformed range, $string, must follow format min..max, min to max, or one value for min/max")
+    if (range.size > 2) error("Malformed range, $string, must follow format min-max, min..max, min to max, or one value for min/max")
 
     // if only one element first and last will be that one element
     return range.first() to range.last()
