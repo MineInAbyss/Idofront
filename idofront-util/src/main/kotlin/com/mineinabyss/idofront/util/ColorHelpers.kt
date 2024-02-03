45d7937bcb308508179068db1d5f9ca8bfe9e030
Boy <sivertpaulsen2@gmail.com>
2024-02-03 17:49:11 +0100

    fix: String#toColor not supporting alpha hex/bit inputs

--- idofront-util/src/main/kotlin/com/mineinabyss/idofront/util/ColorHelpers.kt
+++ idofront-util/src/main/kotlin/com/mineinabyss/idofront/util/ColorHelpers.kt
@@ -5,14 +5,22 @@ import org.bukkit.Color
 /** Converts this string to a [Color] where the string can be formatted with #, 0x and RGB split by ,*/
 fun String.toColor(): Color {
     return when {
-        this.startsWith("#") -> return Color.fromRGB(this.substring(1).toInt(16))
-        this.startsWith("0x") -> return Color.fromRGB(this.substring(2).toInt(16))
+        this.startsWith("#", ignoreCase = true) || this.startsWith("0x", ignoreCase = true) -> {
+            val hexValue = this.substring(if (this[1] == 'x') 2 else 1)
+            val argb = when (hexValue.length) {
+                6 -> 0xFF000000.toInt() or hexValue.toLong(16).toInt()
+                8 -> hexValue.toLong(16).toInt()
+                else -> throw IllegalArgumentException("Invalid hex color format")
+            }
+            Color.fromARGB(argb)
+        }
         "," in this -> {
             val colorString = this.replace(" ", "").split(",")
             if (colorString.any { it.toIntOrNull() == null }) return Color.WHITE
             Color.fromRGB(colorString[0].toInt(), colorString[1].toInt(), colorString[2].toInt())
         }
         //TODO Make this support text, probably through minimessage
-        else -> return Color.WHITE
+        else -> Color.WHITE
     }
 }
+
