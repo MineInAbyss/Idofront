2908f8d721e6d7ef4b9d7e69bdf06aa586848e41
Boy <sivertpaulsen2@gmail.com>
2024-05-12 01:06:37 +0200

    fix: ensure that attribtue-tooltips are hidden when itemflag is set

--- idofront-util/src/main/kotlin/com/mineinabyss/idofront/items/Items.kt
+++ idofront-util/src/main/kotlin/com/mineinabyss/idofront/items/Items.kt
@@ -1,5 +1,8 @@
 package com.mineinabyss.idofront.items
 
+import net.minecraft.core.component.DataComponents
+import org.bukkit.craftbukkit.inventory.CraftItemStack
+import org.bukkit.inventory.ItemFlag
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.ItemMeta
 
@@ -20,4 +23,16 @@ inline fun <reified T : ItemMeta> ItemStack.editItemMeta(apply: T.() -> Unit): I
     apply(meta)
     itemMeta = meta
     return this
+}
+
+/**
+ * In 1.20.5, HIDE_ATTIRBUTES ItemFlag has funky behaviour as the component holds the show_in_tooltip property
+ * This method properly hides the Attributes from the tooltip if an item has the flag set
+ */
+fun ItemStack.hideAttributeTooltipWithItemFlagSet(): ItemStack {
+    if (!hasItemFlag(ItemFlag.HIDE_ATTRIBUTES)) return this
+
+    return CraftItemStack.asCraftCopy(this).apply {
+        handle.set(DataComponents.ATTRIBUTE_MODIFIERS, (handle.get(DataComponents.ATTRIBUTE_MODIFIERS) ?: handle.item.defaultAttributeModifiers).withTooltip(false))
+    }
 }
