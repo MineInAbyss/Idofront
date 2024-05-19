c85d796dcc9b220b5f8e4be126f508be80064299
Boy <sivertpaulsen2@gmail.com>
2024-05-19 23:31:11 +0200

    fix: bad FireworkEffectMeta for ItemMetas#asColorable

--- idofront-util/src/main/kotlin/com/mineinabyss/idofront/items/ItemMetas.kt
+++ idofront-util/src/main/kotlin/com/mineinabyss/idofront/items/ItemMetas.kt
@@ -1,7 +1,9 @@
 package com.mineinabyss.idofront.items
 
 import org.bukkit.Color
+import org.bukkit.FireworkEffect
 import org.bukkit.inventory.meta.FireworkEffectMeta
+import org.bukkit.inventory.meta.FireworkMeta
 import org.bukkit.inventory.meta.ItemMeta
 import org.bukkit.inventory.meta.LeatherArmorMeta
 import org.bukkit.inventory.meta.MapMeta
@@ -42,9 +44,15 @@ fun ItemMeta.asColorable(): Colorable? {
 
         is FireworkEffectMeta -> object : Colorable {
             override var color: Color?
-                get() = meta.color
+                get() = meta.effect?.colors?.firstOrNull()
                 set(value) {
-                    meta.color = value
+                    meta.effect = FireworkEffect.builder()
+                        .withColor(setOf(value ?: meta.effect?.colors ?: listOf(Color.GRAY)))
+                        .with(meta.effect?.type ?: FireworkEffect.Type.BALL)
+                        .withFade(meta.effect?.fadeColors ?: emptyList<Color>())
+                        .trail(meta.effect?.hasTrail() ?: false)
+                        .flicker(meta.effect?.hasFlicker() ?: false)
+                        .build()
                 }
         }
 
