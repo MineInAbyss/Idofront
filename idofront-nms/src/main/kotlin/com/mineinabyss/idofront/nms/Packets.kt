f3ef3e314d4163eaab035d4021b0425e97a6c9d4
Boy <sivertpaulsen2@gmail.com>
2024-06-14 20:09:03 +0200

    fix: expose method with Connection param

--- idofront-nms/src/main/kotlin/com/mineinabyss/idofront/nms/Packets.kt
+++ idofront-nms/src/main/kotlin/com/mineinabyss/idofront/nms/Packets.kt
@@ -21,7 +21,13 @@ import org.bukkit.plugin.java.JavaPlugin
  *  * The lambda should return the modified packet or `null` to prevent it from being sent.
  * @return The modified packet or null to prevent it from sending
  */
+@JvmName("interceptClientboundPlayer")
 fun JavaPlugin.interceptClientbound(key: String = "write_packet_interceptor", intercept: (Packet<*>, Player?) -> Packet<*>?) {
+    PacketListener.interceptClientbound(Key.key(this.name.lowercase(), key.substringAfter(":")), intercept)
+}
+
+@JvmName("interceptClientboundConnection")
+fun JavaPlugin.interceptClientbound(key: String = "write_packet_interceptor", intercept: (Packet<*>, Connection) -> Packet<*>?) {
     PacketListener.interceptClientbound(Key.key(this.name.lowercase(), key.substringAfter(":")), intercept)
 }
 
@@ -34,7 +40,13 @@ fun JavaPlugin.interceptClientbound(key: String = "write_packet_interceptor", in
  *  * The lambda should return the modified packet or `null` to prevent it from being sent.
  * @return The modified packet or null to prevent it from sending
  */
+@JvmName("interceptServerboundPlayer")
 fun JavaPlugin.interceptServerbound(key: String = "read_packet_interceptor", intercept: (Packet<*>, Player?) -> Packet<*>?) {
+    PacketListener.interceptServerbound(Key.key(this.name.lowercase(), key.substringAfter(":")), intercept)
+}
+
+@JvmName("interceptServerboundConnection")
+fun JavaPlugin.interceptServerbound(key: String = "read_packet_interceptor", intercept: (Packet<*>, Connection) -> Packet<*>?) {
     PacketListener.interceptServerbound(Key.key(this.name.lowercase(), key.substringAfter(":")), intercept)
 }
 
@@ -49,7 +61,13 @@ object PacketListener {
      *  * The lambda should return the modified packet or `null` to prevent it from being sent.
      * @return The modified packet or null to prevent it from sending
      */
+    @JvmName("interceptClientboundPlayer")
     fun interceptClientbound(plugin: JavaPlugin, key: String = "write_packet_interceptor", intercept: (Packet<*>, Player?) -> Packet<*>?) {
+        interceptClientbound(Key.key(plugin.name.lowercase(), key.substringAfter(":")), intercept)
+    }
+
+    @JvmName("interceptClientboundConnection")
+    fun interceptClientbound(plugin: JavaPlugin, key: String = "write_packet_interceptor", intercept: (Packet<*>, Connection) -> Packet<*>?) {
         interceptClientbound(Key.key(plugin.name.lowercase(), key.substringAfter(":")), intercept)
     }
 
@@ -62,8 +80,14 @@ object PacketListener {
      *  * The lambda should return the modified packet or `null` to prevent it from being sent.
      * @return The modified packet or null to prevent it from sending
      */
+    @JvmName("interceptServerboundPlayer")
     fun interceptServerbound(plugin: JavaPlugin, key: String = "read_packet_interceptor", intercept: (Packet<*>, Player?) -> Packet<*>?) {
         interceptServerbound(Key.key(plugin.name.lowercase(), key.substringAfter(":")), intercept)
+    }
+
+    @JvmName("interceptServerboundConnection")
+    fun interceptServerbound(plugin: JavaPlugin, key: String = "read_packet_interceptor", interceptConnection: (Packet<*>, Connection) -> Packet<*>?) {
+        interceptServerbound(Key.key(plugin.name.lowercase(), key.substringAfter(":")), interceptConnection)
     }
 
     fun unregisterListener(plugin: JavaPlugin) {
@@ -74,6 +98,7 @@ object PacketListener {
         ChannelInitializeListenerHolder.removeListener(key)
     }
 
+    @JvmName("interceptClientboundPlayer")
     internal fun interceptClientbound(key: Key? = null, intercept: (Packet<*>, Player?) -> Packet<*>?) {
         val key = key ?: Key.key("write_packet_interceptor${ChannelInitializeListenerHolder.getListeners().keys.indexOfLast { it.value().startsWith("write_packet_interceptor") }.plus(1)}")
 
@@ -89,6 +114,23 @@ object PacketListener {
         }
     }
 
+    @JvmName("interceptClientboundConnection")
+    internal fun interceptClientbound(key: Key? = null, intercept: (Packet<*>, Connection) -> Packet<*>?) {
+        val key = key ?: Key.key("write_packet_interceptor${ChannelInitializeListenerHolder.getListeners().keys.indexOfLast { it.value().startsWith("write_packet_interceptor") }.plus(1)}")
+
+        ChannelInitializeListenerHolder.addListener(key) { channel ->
+            channel.pipeline().addBefore("packet_handler", key.asString(), object : ChannelDuplexHandler() {
+                val connection = channel.pipeline()["packet_handler"] as Connection
+                override fun write(ctx: ChannelHandlerContext, packet: Any, promise: ChannelPromise) {
+                    // Player IS nullable, just not marked in Paper
+                    if (packet is Packet<*>) intercept(packet, connection)?.let { ctx.write(it, promise) }
+                    else ctx.write(packet, promise)
+                }
+            })
+        }
+    }
+
+    @JvmName("interceptServerboundPlayer")
     internal fun interceptServerbound(key: Key? = null, intercept: (Packet<*>, Player?) -> Packet<*>?) {
         val key = key ?: Key.key("write_packet_interceptor${ChannelInitializeListenerHolder.getListeners().keys.indexOfLast { it.value().startsWith("read_packet_interceptor") }.plus(1)}")
 
@@ -98,6 +140,22 @@ object PacketListener {
                 override fun channelRead(ctx: ChannelHandlerContext, packet: Any) {
                     // Player IS nullable, just not marked in Paper
                     if (packet is Packet<*>) intercept(packet, connection.player?.bukkitEntity)?.let { ctx.fireChannelRead(it) }
+                    else ctx.fireChannelRead(packet)
+                }
+            })
+        }
+    }
+
+    @JvmName("interceptServerboundConnection")
+    internal fun interceptServerbound(key: Key? = null, intercept: (Packet<*>, Connection) -> Packet<*>?) {
+        val key = key ?: Key.key("write_packet_interceptor${ChannelInitializeListenerHolder.getListeners().keys.indexOfLast { it.value().startsWith("read_packet_interceptor") }.plus(1)}")
+
+        ChannelInitializeListenerHolder.addListener(key) { channel ->
+            channel.pipeline().addBefore("packet_handler", key.asString(), object : ChannelDuplexHandler() {
+                val connection = channel.pipeline()["packet_handler"] as Connection
+                override fun channelRead(ctx: ChannelHandlerContext, packet: Any) {
+                    // Player IS nullable, just not marked in Paper
+                    if (packet is Packet<*>) intercept(packet, connection)?.let { ctx.fireChannelRead(it) }
                     else ctx.fireChannelRead(packet)
                 }
             })
