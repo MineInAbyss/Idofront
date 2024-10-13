e66da09203bbd4c8a471b3c60ad64c4c3f8ad801
Boy <sivertpaulsen2@gmail.com>
2024-10-13 19:41:28 +0200

    feat: add suggestion-methods with tooltips

--- idofront-commands/src/main/kotlin/com/mineinabyss/idofront/commands/brigadier/IdoSuggestionsContext.kt
+++ idofront-commands/src/main/kotlin/com/mineinabyss/idofront/commands/brigadier/IdoSuggestionsContext.kt
@@ -1,5 +1,6 @@
 package com.mineinabyss.idofront.commands.brigadier
 
+import com.mojang.brigadier.Message
 import com.mojang.brigadier.context.CommandContext
 import com.mojang.brigadier.suggestion.SuggestionsBuilder
 import io.papermc.paper.command.brigadier.CommandSourceStack
@@ -20,9 +21,29 @@ data class IdoSuggestionsContext(
         suggestions.suggest(name)
     }
 
+    /** Add a suggestion with a tooltip */
+    fun suggest(name: String, tooltip: () -> String) {
+        suggestions.suggest(name, tooltip)
+    }
+
+    /** Add a suggestion with a tooltip */
+    fun suggest(name: String, tooltip: Message) {
+        suggestions.suggest(name, tooltip)
+    }
+
     /** Add a list of suggestions. */
     fun suggest(list: List<String>) {
         list.forEach { suggest(it) }
+    }
+
+    /** Add a list of suggestions with a tooltip. */
+    fun suggest(list: List<String>, tooltip: () -> String) {
+        list.forEach { suggest(it, tooltip) }
+    }
+
+    /** Add a list of suggestions with a tooltip. */
+    fun suggest(list: List<String>, tooltip: Message) {
+        list.forEach { suggest(it, tooltip) }
     }
 
     /** Add a suggestion, filtering it as the user types. */
@@ -31,8 +52,30 @@ data class IdoSuggestionsContext(
             suggest(name)
     }
 
+    /** Add a suggestion with a tooltip, filtering it as the user types. */
+    fun suggestFiltering(name: String, tooltip: () -> String) {
+        if (name.startsWith(argument, ignoreCase = true))
+            suggest(name, tooltip)
+    }
+
+    /** Add a suggestion with a tooltip, filtering it as the user types. */
+    fun suggestFiltering(name: String, tooltip: Message) {
+        if (name.startsWith(argument, ignoreCase = true))
+            suggest(name, tooltip)
+    }
+
     /** Add a list of suggestions, filtering them as the user types. */
     fun suggestFiltering(list: List<String>) {
         list.forEach { suggestFiltering(it) }
+    }
+
+    /** Add a list of suggestions with a tooltip, filtering them as the user types. */
+    fun suggestFiltering(list: List<String>, tooltip: () -> String) {
+        list.forEach { suggestFiltering(it, tooltip) }
+    }
+
+    /** Add a list of suggestions with a tooltip, filtering them as the user types. */
+    fun suggestFiltering(list: List<String>, tooltip: Message) {
+        list.forEach { suggestFiltering(it, tooltip) }
     }
 }
