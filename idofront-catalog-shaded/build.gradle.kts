433b7aa8f11510563c0e3bd2ec8a9efc6f60fb4b
Danielle Voznyy <dan.voznyy@gmail.com>
2024-12-21 01:36:17 -0500

    fix: I dont know where build.gradle my diff went but I found it

--- idofront-catalog-shaded/build.gradle.kts
+++ idofront-catalog-shaded/build.gradle.kts
@@ -1,12 +1,28 @@
+import me.champeau.gradle.japicmp.JapicmpTask
+import kotlin.io.path.listDirectoryEntries
+
 plugins {
     id("com.mineinabyss.conventions.kotlin.jvm")
     id("com.mineinabyss.conventions.copyjar")
     id("com.mineinabyss.conventions.papermc")
     id("com.mineinabyss.conventions.nms")
+    id("me.champeau.gradle.japicmp") version "0.4.5"
 }
 
 val libs = rootProject.extensions.getByType<VersionCatalogsExtension>().named("libs")
 
+tasks {
+    task<JapicmpTask>("checkBreakingChanges") {
+        oldClasspath.from(rootProject.file("past-releases").toPath().listDirectoryEntries().first())
+        newClasspath.from(shadowJar)
+        onlyBinaryIncompatibleModified = true
+        includeSynthetic = true
+        ignoreMissingClasses = true
+        failOnModification = true
+        packageIncludes = listOf("com.mineinabyss.*")
+        mdOutputFile = layout.buildDirectory.file("reports/japi.md")
+    }
+}
 dependencies {
     libs.findBundle("platform").get().get().forEach {
         implementation(it)
