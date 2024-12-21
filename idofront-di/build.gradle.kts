e4d67d871c2b5e6d273f724397a2a72d15e5dd87
Danielle Voznyy <dan.voznyy@gmail.com>
2024-12-21 01:52:11 -0500

    fix: Apply version to subprojects correctly refactor: Swap Idofront-DI to be JVM-only since we are planning to phase it out and Geary does not use it anymore

--- idofront-di/build.gradle.kts
+++ idofront-di/build.gradle.kts
@@ -1,15 +1,8 @@
 plugins {
-    id("com.mineinabyss.conventions.kotlin.multiplatform")
+    id("com.mineinabyss.conventions.kotlin.jvm")
     id("com.mineinabyss.conventions.publication")
-    id("com.mineinabyss.conventions.multiplatform.testing")
+    id("com.mineinabyss.conventions.testing")
 }
-
-kotlin {
-    jvm()
-    js {
-        browser()
-        nodejs()
-    }
-    mingwX64()
-    linuxX64()
+dependencies {
+    testImplementation(kotlin("test"))
 }
