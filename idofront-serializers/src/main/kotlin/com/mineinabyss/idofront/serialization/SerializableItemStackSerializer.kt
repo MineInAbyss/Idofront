0a6ae375c56ad433e577eb3a83d1d3d6a6049d54
Boy <sivertpaulsen2@gmail.com>
2025-03-08 13:34:13 +0100

    Revert "refactor: Use new @KeepGeneratedSerializer to avoid BaseSerializableItemStack split"
    This reverts commit cffb07f7bbb88d18b31e9ecb36fb69755d13681a.

--- idofront-serializers/src/main/kotlin/com/mineinabyss/idofront/serialization/SerializableItemStackSerializer.kt
+++ idofront-serializers/src/main/kotlin/com/mineinabyss/idofront/serialization/SerializableItemStackSerializer.kt
@@ -1,45 +1,56 @@
 package com.mineinabyss.idofront.serialization
 
 import com.charleskorn.kaml.YamlInput
-import com.charleskorn.kaml.YamlNode
+import com.charleskorn.kaml.YamlList
 import com.charleskorn.kaml.YamlScalar
+import kotlinx.serialization.ContextualSerializer
+import kotlinx.serialization.InternalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.encoding.decodeStructure
 import org.bukkit.Material
 
 class SerializableItemStackSerializer : KSerializer<SerializableItemStack> {
     //    override val descriptor: SerialDescriptor = BaseSerializableItemStack.serializer().descriptor
-    override val descriptor: SerialDescriptor = YamlNode.serializer().descriptor
+    override val descriptor: SerialDescriptor =
+        ContextualSerializer(BaseSerializableItemStack::class).descriptor
 
     override fun deserialize(decoder: Decoder): SerializableItemStack {
-        val node = (decoder as? YamlInput)?.node
+        val node = (decoder as? YamlInput)?.node ?: return BaseSerializableItemStack.serializer().deserialize(decoder)
         if (node is YamlScalar) {
             return decodeFromShorthand(node.content)
         }
-        return decoder.decodeSerializableValue(SerializableItemStack.generatedSerializer())
+        val structure = decoder.beginStructure(descriptor) as YamlInput
+        val innerNode = structure.node
+        if (innerNode is YamlScalar) {
+            return decodeFromShorthand(innerNode.content)
+        }
+        return structure.decodeInline(BaseSerializableItemStack.serializer().descriptor).decodeSerializableValue(BaseSerializableItemStack.serializer())
+            .also { structure.endStructure(BaseSerializableItemStack.serializer().descriptor) }
+
     }
 
     fun decodeFromShorthand(stringShorthand: String) = when {
-        stringShorthand.startsWith("minecraft:") -> SerializableItemStack(
+        stringShorthand.startsWith("minecraft:") -> BaseSerializableItemStack(
             type = Material.matchMaterial(
                 stringShorthand
             )
         )
 
-        stringShorthand.startsWith("crucible ") -> SerializableItemStack(
+        stringShorthand.startsWith("crucible ") -> BaseSerializableItemStack(
             crucibleItem = stringShorthand.removePrefix("crucible ")
         )
 
-        stringShorthand.startsWith("nexo ") -> SerializableItemStack(
+        stringShorthand.startsWith("nexo ") -> BaseSerializableItemStack(
             nexoItem = stringShorthand.removePrefix("nexo ")
         )
 
-        else -> SerializableItemStack(prefab = stringShorthand)
+        else -> BaseSerializableItemStack(prefab = stringShorthand)
     }
 
     override fun serialize(encoder: Encoder, value: SerializableItemStack) {
-        SerializableItemStack.serializer().serialize(encoder, value)
+        BaseSerializableItemStack.serializer().serialize(encoder, value)
     }
 }
