89e7678cf35371fc3d1de0ebe668b5c95a7a6fc2
HiFlii <hiflimc@gmail.com>
2025-09-03 20:12:54 -0400

    fix: Unbreakable now supported in toItemStack

--- idofront-serializers/src/main/kotlin/com/mineinabyss/idofront/serialization/SerializableItemStack.kt
+++ idofront-serializers/src/main/kotlin/com/mineinabyss/idofront/serialization/SerializableItemStack.kt
@@ -233,6 +233,8 @@ data class BaseSerializableItemStack(
         potDecorations?.setDataType(applyTo)
 
         SerializableDataTypes.setData(applyTo, DataComponentTypes.INTANGIBLE_PROJECTILE, intangibleProjectile)
+        SerializableDataTypes.setData(applyTo, DataComponentTypes.GLIDER, glider)
+        SerializableDataTypes.setData(applyTo, DataComponentTypes.UNBREAKABLE, unbreakable)
 
         return applyTo
     }
@@ -318,6 +320,7 @@ fun ItemStack.toSerializable(): SerializableItemStack = with(itemMeta) {
         potDecorations = dataIfOverriden(DataComponentTypes.POT_DECORATIONS)?.let(SerializableDataTypes::PotDecorations),
 
         intangibleProjectile = SerializableDataTypes.IntangibleProjectile.takeIf { hasData(DataComponentTypes.INTANGIBLE_PROJECTILE) },
+        glider = SerializableDataTypes.Glider.takeIf { hasData(DataComponentTypes.GLIDER) },
         unbreakable = SerializableDataTypes.Unbreakable.takeIf { hasData(DataComponentTypes.UNBREAKABLE) },
 
     )
